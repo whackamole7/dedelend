62a66eb3179bb5620b4f79f0f3f93e12877b34a3
whackamole7 <whackamole7@proton.me>
2022-09-20 13:10:42 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+$(function() {
+	const isMobile = $(window).width() <= 767;
+
+	
+});
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxuXG4kKGZ1bmN0aW9uKCkge1xuXHRjb25zdCBpc01vYmlsZSA9ICQod2luZG93KS53aWR0aCgpIDw9IDc2NztcblxuXHRcbn0pO1xuXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
