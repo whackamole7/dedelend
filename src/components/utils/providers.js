fbe09e5044f8ccae6282117271e2bb7cf111e5b7
whackamole7 <whackamole7@proton.me>
2022-09-27 20:20:22 +0300

    Prod contracts

--- src/components/utils/providers.js
+++ src/components/utils/providers.js
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers';
 
-export const netProvider = new ethers.getDefaultProvider('https://arb-mainnet.g.alchemy.com/v2/5ZdGJb2c1DWObl73ERVIL3PTUl6UCMRA');
+export const netProvider = new ethers.getDefaultProvider('https://arb-mainnet.g.alchemy.com/v2/r6kd0heyoOCe8RFndkyv0XW0F-BG38Fy');
 
 
 export const mmProvider = window.ethereum ? new ethers.providers.Web3Provider(window.ethereum) : undefined;
