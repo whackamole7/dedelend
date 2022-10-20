83c0ae94daa25c4d9858a0e3a38b69c470e46f84
whackamole7 <whackamole7@proton.me>
2022-10-20 17:09:45 +0300

    Updates

---
+++ static/js/11.6bb7187f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdedelend=this.webpackJsonpdedelend||[]).push([[11],{694:function(e,d){}}]);
+//# sourceMappingURL=11.6bb7187f.chunk.js.map
--- static/js/11.da71f1a5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdedelend=this.webpackJsonpdedelend||[]).push([[11],{944:function(e,d){}}]);
-//# sourceMappingURL=11.da71f1a5.chunk.js.map
